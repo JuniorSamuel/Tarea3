23f977e3bb9e5ca21fa80884ad2f48e0f698dcf9
Junior Samuel De Los Santos Velazquez <juniorvelazquezmt19@gmail.com>
2022-02-08 21:05:43 -0400

    Actulizacion en la sumadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -7,11 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public appPages = [
     { title: 'Inicio', url: 'start/', icon: 'home' },
-    { title: 'Suma', url: 'suma/', icon: 'calculator' },
+    { title: 'Sumadora', url: 'suma/', icon: 'calculator' },
     { title: 'Numero-Texto', url: 'translate-num/', icon: 'book' },
     { title: 'Multiplicatión', url: 'multiplication/', icon: 'calculator' },
     { title: 'Experiencia', url: 'youtube-view/', icon: 'videocam' },
   ];
 
-  constructor() {}
+  constructor() { }  
 }
--- src/app/pages/sum/sum.page.ts
+++ src/app/pages/sum/sum.page.ts
@@ -1,12 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Storage } from '@ionic/storage-angular';
+import { Component } from '@angular/core';
 
+import { Storage } from '@ionic/storage-angular';
 export interface value {
   key: string,
-  value: {
-    num1: number,
-    num2: number
-  }
+  value: string
 }
 
 @Component({
@@ -17,19 +14,20 @@ export interface value {
 })
 export class SumPage {
 
+  operation: string = '+';
   value = { num1: null, num2: null }
   results: value[] = [];
   key: number = 0;
+  result: number = 0;
 
   constructor(private storage: Storage) { this.init() }
 
-  calculate() {// Registramos los valores en la base de datos
+  save() {// Registramos los valores en la base de datos
     if (this.value.num1 != null && this.value.num2 != null) { // validamos los datos
-      let item: value = { key: `${++this.key}`, value: { num1: this.value.num1, num2: this.value.num2 } };
+      let item: value = { key: `${++this.key}`, value: `${this.value.num1} ${this.operation} ${this.value.num2} = ${this.getOperation(this.value.num1, this.value.num2)}`};
       this.results.unshift(item);
       this.set(item);
     }
-    this.value = {num1: null, num2: null}; //Reseteamos los valores
   }
 
   delete(v: value) {
@@ -41,20 +39,36 @@ export class SumPage {
     const storage = await this.storage.create();
     this.storage = storage;
     this.key = await this.getkey();
-    storage.forEach((value: any, key: any, index: any) => {
-      this.results.unshift({key: key, value: value});
+    storage.forEach((value: any, key: any) => {
+      this.results.unshift({ key: key, value: value});
     });
-  
+
+  }
+
+  selectButton(op: string) {
+    this.operation = op;
   }
 
   //Obtenemos en el valor de la ultima llava guardada en el IndexDB
-  async getkey(){
+  async getkey() {
     let lenght = await this.storage.length();;
     let keys = await this.storage.keys();
-    console.log(keys[lenght-1] || "0")
-    return parseInt(keys[lenght-1] || "0");
+    console.log(keys[lenght - 1] || "0")
+    return parseInt(keys[lenght - 1] || "0");
   }
 
+  getOperation(num1: number, num2: number) {
+    switch (this.operation) {
+      case "+":
+        return num1 + num2;
+      case "-":
+        return num1 - num2;
+      case "/":
+        return num2 !== 0  ? num1 / num2 : 'Indefinido';
+      case "x":
+        return num1 * num2;
+    }
+  }
 
   public async set(value: value) {
     await this.storage?.set(value.key, value.value);
